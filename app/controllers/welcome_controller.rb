1ff3a5930e800cd6bc54569e5882575b2390fcfc
hdotson <henry.d.dotson@gmail.com>
2015-03-20 11:54:22 -0400

    Filters working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,13 +7,18 @@ class WelcomeController < ApplicationController
     @materials_grid = initialize_grid(Material,
       :include => [:space, :customer, :building],
       :conditions => {:customer => @customer},
-      :per_page => 10
+      :per_page => 10,
+      :enable_export_to_csv => true,
+      :csv_file_name => 'Survey Data'
+
       )
 	else
   	 
     @materials_grid = initialize_grid(Material,
       :include => [:space, :customer, :building],
-      :per_page => 10
+      :per_page => 10,
+      :enable_export_to_csv => true,
+      :csv_file_name => 'Survey Data'
       )
   	end
    end
--- app/models/material.rb
+++ app/models/material.rb
@@ -8,7 +8,7 @@ belongs_to :customer
 ACM_DICT = {1=> 'Sampled, ACM', 2 => 'Sampled, Non-ACM', 3 => 'Assumed ACM', 4 => 'Non-suspect material', 5 =>'No access'}
 
 def acm_display
-ACT_DICT[self.acm_category]
+ACM_DICT[self.acm_category]
 end
 
 MAT_DICT = {1=> 'Thermal System Insulation', 2 => 'Surfacing', 3 => 'Miscellaneous', 4 => 'Not applicable'}
@@ -20,7 +20,7 @@ end
 DAM_DICT = {1=> 'Localized', 2 => 'Distributed'}
 
 def dam
-DAM_DICT[self.typedamage]
+DAM_DICT[self.type_damage]
 end
 
 end
