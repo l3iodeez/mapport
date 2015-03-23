1990055736616d445198028b13d00de912b8bbbb
hdotson <support@nyenvironmental.com>
2015-03-23 00:21:28 -0400

    recovery from stupid

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,6 @@
 class Report < ActiveRecord::Base
   belongs_to :customer
+  belongs_to :building
   mount_uploader :pdf, PdfUploader 
   
   validates_presence_of :reportname, :description, :filename, :customer_id, :report_date
