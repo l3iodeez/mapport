f74c15b907073a8066e5504a5d2875351c57d7b7
hdotson <henry.d.dotson@gmail.com>
2015-05-08 10:13:18 -0400

    Added delayed_job

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   add_flash_types :error, :failure
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   before_filter :set_g_user
   before_action :authenticate_user!
  
