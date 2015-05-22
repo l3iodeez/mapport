12a5fc039f2800b214ed33cbc95ad7aa8b47618b
Henry Dotson <henry.d.dotson@gmail.com>
2015-05-22 15:02:58 -0400

    err

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
             @user = current_user
             unless @user.is_admin  #check if current user is an administrator
 	          #redirect_to root_path, 
-	          alert: "You are not an admin." # boot them to the main page if not admin
+	          #alert: "You are not an admin." # boot them to the main page if not admin
 	        else
            		# allow user to load admin only pages
             end
