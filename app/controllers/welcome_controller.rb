bede8036cf44fbfc07aa93bdadcef6e234da24c4
Henry Dotson <henry.d.dotson@gmail.com>
2015-05-22 14:10:00 -0400

    err

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,28 +8,9 @@ def index
 	@curruser = current_user
 	@currcustomer = current_user.customer
 	@customers = Customer.all
-	if current_user.is_admin 
-	@buildings = Building.all
-	else
+	
 	@buildings = Building.all.where(customer: @currcustomer)	
-	end
-
-
-		 @buildings_grid = initialize_grid(@buildings,
-		      :include => [:customer],
-		     
-		      :name => 'buildings',
-		      :per_page => 10,
-		      :enable_export_to_csv => false,
-		      
-		      )
-
-		@hash = Gmaps4rails.build_markers(@buildings) do |building, marker|
-		  marker.lat building.latitude
-		  marker.lng building.longitude
-		  marker.infowindow view_context.link_to(building.buildingname, show_building_path(building.id))
-		end
-
+	
 
 end
 
@@ -68,7 +49,7 @@ end
 def check_ownership
 	@user = current_user
 	@building = Building.find_by_id(params[:building_id])
-      if @user.customer_id != @building.customer_id  #check if current user is a user tied to report's owner
+      if @user.customer_id != @building.customer_id && !current_user.is_admin #check if current user is a user tied to report's owner
          redirect_to root_path,
           alert: "You are not authorized to select this facility" # boot them to the main page if not admin
       end
